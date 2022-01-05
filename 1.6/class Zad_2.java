7274fb39b15d900337c6a2e7ec461477dad101fb
KacperLewa <kacperlewandowski491@gmail.com>
2022-01-05 09:05:11 +0100

    l

---
+++ 1.6/class Zad_2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Zad_2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int a = s.length();
+        System.out.println("Wprowadziles "+a+" znakow.");
+    }
+}
