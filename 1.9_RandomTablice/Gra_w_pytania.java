b153e1b3d56c952b319a13c3a60ec398c28b288c
KacperLewa <kacperlewandowski491@gmail.com>
2022-01-12 09:34:54 +0100

    d

---
+++ 1.9_RandomTablice/Gra_w_pytania.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+class Gra_w_pytania{
+    public static void main(String[] args){
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+        String [] tab = new String[100];
+        
+    }
+}
