f65acbde00ea1dfa6a1d1ab829c14cd1f5e8965c
KacperLewa <kacperlewandowski491@gmail.com>
2022-01-13 14:19:29 +0100

    s

---
+++ 1.9_RandomTablice/KacperLewandowski.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Random;
+class KacperLewandowski{
+    public static void main(String[] args){
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+        String [] tab = new String[100];
+        tab[0] = "W którym roku wybuchła II wojna światowa?";
+        tab[1] = "W którym roku zakończyła się II wojna światowa?";
+        tab[2] = "W którym roku wybuchła I wojna światowa?";
+        tab[3] = "W którym roku zakończyła się I wojna światowa?";
+        tab[4] = "W którym roku odbyła się bitwa pod Grunwaldem?";
+        tab[5] = "W którym roku rozpoczą się potop szwedzki?";
+        tab[6] = "W którym roku polska przyjeła chrzest?";
+        tab[7] = "W którym roku odbyło się powstanie warszawskie?";
+        tab[8] = "W którym roku podpisano konstytucję 3 Maja?";
+        tab[9] = "W którym roku PNE odbyły się pierwsze igrzyska olimpijskie?";
+        tab[10] = "W którym roku PNE Założono Rzym?";
+        tab[11] = "W którym roku chrześcijanizm stał się wolną religią?";
+        tab[12] = "W którym roku odbył się zjazd gnieźnieński?";
+        tab[13] = "W którym roku odbyła się schizma wschodnia?";
+        tab[14] = "W którym roku odbyła się bitwa pod Legnicą?";
+        tab[15] = "W którym roku Kazimierz Wielki założył Akademię Krakowską?";
+        tab[16] = "W którym roku powstała unia polsko-litewska?";
+        tab[17] = "W którym roku zawarto pokój w Brześciu (Krzyżacy i Polska)?";
+        
+    }
+}
