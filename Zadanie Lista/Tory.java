fd53924882b621baba1f6496ca6c15d4da976444
KacperLewa <kacperlewandowski491@gmail.com>
2022-06-08 07:09:25 +0200

    projekt

--- Zadanie Lista/Tory.java
+++ Zadanie Lista/Tory.java
@@ -3,5 +3,23 @@ class Tory{
     private String data;
     private String wygrany;
 
-    
+    public Tory(String tor, String data, String wygrany){
+        this.tor = tor;
+        this.data = data;
+        this.wygrany = wygrany;
+    }
+    public String getTor(){
+        return tor;
+    }
+    public String getData(){
+        return data;
+    }
+    public String getWygrany(){
+        return wygrany;
+    }
+    public void menu(){
+        System.out.println("1. Wyświetl wszystkie tory.");
+        System.out.println("2. Wyświetl wszystkich zwycięzców.");
+        System.out.println("3. Wyświetl kto wygrał najwięcej wyścigów.");
+    }
 }
